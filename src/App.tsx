2785a4c4f4d904ea712bd81788b4baaeae1bd42f
wuxuwei <2509165479@qq.com>
2024-01-09 21:52:44 +0800

    chore(deps): add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
+import { StyleProvider } from '@ant-design/cssinjs';
+import { Button, ConfigProvider, theme, App as AntdApp } from 'antd';
+import zhCN from 'antd/locale/zh_CN';
+
 import styles from './app.module.css';
 function App() {
   return (
-    <div className={styles.app}>
-      <div className={styles.container}>
-        Hello World，这是<span>React课程第一节</span>
-      </div>
-    </div>
+    <ConfigProvider
+      locale={zhCN}
+      theme={{
+        algorithm: theme.defaultAlgorithm,
+        token: {
+          colorPrimary: '#00B96B',
+        },
+        components: {
+          Layout: {
+            colorBgBody: '',
+          },
+        },
+      }}>
+      <StyleProvider hashPriority="high">
+        <AntdApp>
+          <div className={styles.app}>
+            <div className={styles.container}>
+              Hello World，这是<span>React课程第一节</span>
+              <Button
+                type="primary"
+                className="!bg-lime-400 !text-emerald-900"
+                target="_blank">
+                点此打开
+              </Button>
+            </div>
+          </div>
+        </AntdApp>
+      </StyleProvider>
+    </ConfigProvider>
   );
 }
 
